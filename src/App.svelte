cc9eb81bdc979367c024764eefe9d95daf234c8b
Daigo Shitara <stain.witness@gmail.com>
2020-04-28 16:58:07 +0900

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { onMount } from 'svelte'
+  import mapboxgl from 'mapbox-gl'
+
+  mapboxgl.accessToken = MAPBOX_TOKEN
+
+  onMount(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/dark-v10',
+      renderWorldCopies: false,
+    })
+  })
+</script>
+
+<style>
+  main {
+    height: 100%;
+  }
+  #map {
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
+<main>
+  <div id="map" />
+</main>
